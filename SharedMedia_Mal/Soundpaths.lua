ea6189bba90c94eec108300e576b4381bae7b644
chrisbrigs9001 <chrisbrigs9001@gmail.com>
2024-08-30 16:49:22 -0500

    Add files via upload

---
+++ SharedMedia_Mal/Soundpaths.lua
@@ -0,0 +1,40 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFFFF0000onezyMeow|r", [[Interface\Addons\SharedMedia_Mal\sound\onezyMeow.ogg]])
+LSM:Register("sound", "|cFFFF0000growl_crying|r", [[Interface\Addons\SharedMedia_Mal\sound\growl_crying.ogg]])
+LSM:Register("sound", "|cFFFF0000sm64_mario_burned|r", [[Interface\Addons\SharedMedia_Mal\sound\sm64_mario_burned.ogg]])
+LSM:Register("sound", "|cFFFF0000sm64_mario_lets_go|r", [[Interface\Addons\SharedMedia_Mal\sound\sm64_mario_lets_go.ogg]])
+LSM:Register("sound", "|cFFFF0000sm64_mario_mamma-mia|r", [[Interface\Addons\SharedMedia_Mal\sound\sm64_mario_mamma-mia.ogg]])
+LSM:Register("sound", "|cFFFF0000sm64_mario_waha|r", [[Interface\Addons\SharedMedia_Mal\sound\sm64_mario_waha.ogg]])
+LSM:Register("sound", "|cFFFF0000mario_hurry_up|r", [[Interface\Addons\SharedMedia_Mal\sound\smb3_hurry_up.ogg]])
+LSM:Register("sound", "|cFFFF0000mario_1-up|r", [[Interface\Addons\SharedMedia_Mal\sound\smb3_1-up.ogg]])
+LSM:Register("sound", "|cFFFF0000mario_starman_theme|r", [[Interface\Addons\SharedMedia_Mal\sound\ssbm_starman_theme.ogg]])
+LSM:Register("sound", "|cFFFF0000mario_hammer_theme|r", [[Interface\Addons\SharedMedia_Mal\sound\ssbm_hammer_theme.ogg]])
+LSM:Register("sound", "|cFFFF0000mario_22_16|r", [[Interface\Addons\SharedMedia_Mal\sound\ssbm_dr_mario_22_mario_16.ogg]])
+LSM:Register("sound", "|cFFFF0000mario_dr_10_06_05|r", [[Interface\Addons\SharedMedia_Mal\sound\ssbm_dr_mario_10_luigi_06_mario_05.ogg]])
+LSM:Register("sound", "|cFFFF0000verune_abuse|r", [[Interface\Addons\SharedMedia_Mal\sound\verune_abuse.ogg]])
+LSM:Register("sound", "|cFFFF0000yipz_planDeath|r", [[Interface\Addons\SharedMedia_Mal\sound\yipz_planDeath.ogg]])
+LSM:Register("sound", "|cFFFF0000fuckass|r", [[Interface\Addons\SharedMedia_Mal\sound\fuckass.ogg]])
+LSM:Register("sound", "|cFFFF0000uwu|r", [[Interface\Addons\SharedMedia_Mal\sound\uwu_ogg.ogg]])
+LSM:Register("sound", "|cFFFF0000Dorki_Loves_Ion_Hazikostas|r", [[Interface\Addons\SharedMedia_Mal\sound\dorki_sucks_ions_dick.ogg]])
+LSM:Register("sound", "|cFFFF0000Move Bitch|r", [[Interface\Addons\SharedMedia_Mal\sound\move_bitch.ogg]])
+LSM:Register("sound", "|cFFFF0000TheDarkness|r", [[Interface\Addons\SharedMedia_Mal\sound\theDarkness.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
